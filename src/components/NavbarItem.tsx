d65e541c874f91c486c38b52437d3026ff3bfe1a
rizeenf <rize.nf11@gmail.com>
2024-01-05 00:51:17 +0700

    initial commit

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { PRODUCT_CATEGORY } from "@/config";
+import Image from "next/image";
 
 type Category = typeof PRODUCT_CATEGORY;
 
@@ -31,13 +32,28 @@ const NavbarItem = ({
         </Button>
       </div>
       {activeIndex == idx ? (
-        <div className="absolute top-full h-full inset-x-0 px-10 bg-white text-black">
+        <div className="absolute top-full  inset-x-0 px-10 bg-black bg-opacity-60 text-white">
           <div className="absolute top-1/2 inset-0" aria-hidden />
-          {product.lists.map((item) => (
-            <div className="relative" key={item.title}>
-              <span>{item.title}</span>
-            </div>
-          ))}
+          <div className="mx-auto max-w-7xl py-14 flex flex-row gap-5">
+            {product.lists.map((item) => (
+              <div className="relative group" key={item.title}>
+                <div className="p-2 border rounded border-white flex flex-col group-hover:opacity-90 text-muted shadow-lg">
+                  <div className="relative aspect-video overflow-hidden rounded-sm group-hover:opacity-90 shadow">
+                    <Image
+                      alt={item.title}
+                      src={item.image}
+                      width={300}
+                      height={300}
+                    />
+                  </div>
+                  <span>{item.title}</span>
+                  <span className="text-sm text-muted-foreground">
+                    See details &rarr;
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       ) : null}
     </div>
