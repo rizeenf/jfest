66cabef849e187267f3f24d850fd82ae4ef5c3b9
rizeenf <rize.nf11@gmail.com>
2024-01-15 20:39:03 +0700

    fix: type NODE_BUILD to NEXT_BUILD

--- dist/server.js
+++ dist/server.js
@@ -62,10 +62,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 var express_1 = __importDefault(require("express"));
 var get_payload_1 = require("./get-payload");
 var next_utils_1 = require("./next-utils");
+var trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 var trpc_1 = require("./trpc");
 var body_parser_1 = __importDefault(require("body-parser"));
 var webhooks_1 = require("./webhooks");
@@ -78,29 +78,30 @@ var createContext = function (_a) {
     return ({ req: req, res: res });
 };
 var start = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var payload, webhookMiddleware;
+    var webhookMiddleware, payload;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, (0, get_payload_1.getPayloadClient)({
-                    initOptions: {
-                        express: app,
-                        onInit: function (cms) { return __awaiter(void 0, void 0, void 0, function () {
-                            return __generator(this, function (_a) {
-                                cms.logger.info("Admin URL ".concat(cms.getAdminURL()));
-                                return [2 /*return*/];
-                            });
-                        }); },
-                    },
-                })];
-            case 1:
-                payload = _a.sent();
+            case 0:
                 webhookMiddleware = body_parser_1.default.json({
                     verify: function (req, _, buffer) {
                         req.rawBody = buffer;
                     },
                 });
                 app.use("/api/webhooks/stripe", webhookMiddleware, webhooks_1.stripeWebhookHandler);
-                if (process.env.NODE_BUILD) {
+                return [4 /*yield*/, (0, get_payload_1.getPayloadClient)({
+                        initOptions: {
+                            express: app,
+                            onInit: function (cms) { return __awaiter(void 0, void 0, void 0, function () {
+                                return __generator(this, function (_a) {
+                                    cms.logger.info("Admin URL ".concat(cms.getAdminURL()));
+                                    return [2 /*return*/];
+                                });
+                            }); },
+                        },
+                    })];
+            case 1:
+                payload = _a.sent();
+                if (process.env.NEXT_BUILD) {
                     app.listen(PORT, function () { return __awaiter(void 0, void 0, void 0, function () {
                         return __generator(this, function (_a) {
                             switch (_a.label) {
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import { inferAsyncReturnType } from "@trpc/server";
-import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
+import { inferAsyncReturnType } from "@trpc/server";
 import bodyParser from "body-parser";
 import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhooks";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -22,6 +24,14 @@ export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
 
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
+  app.use("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -31,22 +41,14 @@ const start = async () => {
     },
   });
 
-  const webhookMiddleware = bodyParser.json({
-    verify: (req: WebhookRequest, _, buffer) => {
-      req.rawBody = buffer;
-    },
-  });
-
-  app.use("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
-
-  if (process.env.NODE_BUILD) {
+  if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info("NextJS is build for production");
 
       // @ts-expect-error
       await nextBuild(path.join(__dirname, "../"));
 
-      process.exit()
+      process.exit();
     });
 
     return;
